93fbb14027a8d8ad7e27f3f26429d7d6719783e9
EloyChavezDev <laneloychavez@gmail.com>
2024-02-26 08:06:28 -0600

    solucion basico

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 document.getElementById('boton2').addEventListener('click',function(){
     alert('Hola mundo');
 })
+
+//Solucion 1
+
+const showName = () => {
+    const name = document.querySelector('#name').value;
+    window.alert(`Nombre: ${name}`);
+  };
+  
+  const button = document.querySelector('.button button2');
+  button.addEventListener('click', showName);
