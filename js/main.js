25bf37d1e907873dfd4a5ddca23ac57950fb3c9e
EloyChavezDev <laneloychavez@gmail.com>
2024-02-27 07:46:16 -0600

    innerHTML

--- js/main.js
+++ js/main.js
@@ -57,4 +57,10 @@ function mostrarDatos() {
   }
 
 
-  
+  //Cambiar contenido con innerHTML
+  function cambiarContenido(){
+    var dir = document.getElementById("miDiv");
+
+    //modificando contenido
+    dispatchEvent.innerHTML = "<h1>Nuevo Titulo</h1><p>Contenido Nuevo</p>"
+  }
