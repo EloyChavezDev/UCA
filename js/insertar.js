2b2333e20b32f0ae418a7d495e2f9bc3b8813486
EloyChavezDev <laneloychavez@gmail.com>
2024-02-27 08:13:27 -0600

    createTextNode

--- js/insertar.js
+++ js/insertar.js
@@ -2,5 +2,23 @@
 function cambiarContenido(){
     var div = document.getElementById("miDiv");
     //modificando contenido
-    div.innerHTML = "<h1>Nuevo Titulo</h1><p>Contenido Nuevo</p>";
+    div.innerHTML = "<h1>Nuevo Titulo hecho con innerHTML</h1><p>Contenido Nuevo con innerHTML</p>";
   }
+
+//Cambiar con createTextNode
+function cambiarContenido2(){
+    var div = document.getElementById("miDiv");
+
+    //creamos nuevo nodo de texto
+    var nuevoTexto = document.createTextNode("Titulo Nuevo 2 hecho con createTextNode");
+
+    //Agregamos el nodo al div
+    div.appendChild(nuevoTexto);
+
+    //Creamos otro nodo de texto
+    var nuevoParrafo = document.createElement("p");
+    nuevoParrafo.textContent = "Contenido nuevo con createTextNode";
+
+    //Agregamos el parrafo al div
+    div.appendChild(nuevoParrafo);
+}
