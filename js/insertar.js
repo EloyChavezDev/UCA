c14a19c51e991706ef3ab66b02d3ba8e52631e81
EloyChavezDev <laneloychavez@gmail.com>
2024-02-27 08:02:29 -0600

    Update insertar.js
    typos

--- js/insertar.js
+++ js/insertar.js
@@ -1,6 +1,6 @@
 //Cambiar contenido con innerHTML
 function cambiarContenido(){
-    var dir = document.getElementById("miDiv");
+    var div = document.getElementById("miDiv");
     //modificando contenido
-    dispatchEvent.innerHTML = "<h1>Nuevo Titulo</h1><p>Contenido Nuevo</p>";
+    div.innerHTML = "<h1>Nuevo Titulo</h1><p>Contenido Nuevo</p>";
   }
