74e7ba0cb00e4c654e4062b33de0e61826982a21
EloyChavezDev <laneloychavez@gmail.com>
2024-03-09 11:45:31 -0600

    Edit Example
    Agregamos CSS

--- js/productos.js
+++ js/productos.js
@@ -1,11 +1,26 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://eloychavezdev.github.io/UCA/JSON/productos.json');
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    const productos = JSON.parse(xhr.responseText);
-    // Aquí puedes usar la información de los productos
-  } else {
-    console.error('Error al cargar la base de datos');
-  }
-};
-xhr.send();
+document.addEventListener("DOMContentLoaded", function () {
+    // Datos de la base de datos en formato JSON
+    var users = [
+        { id: 1, name: "Juan", age: 25 },
+        { id: 2, name: "María", age: 30 },
+        { id: 3, name: "Pedro", age: 22 }
+    ];
+
+    // Función para mostrar usuarios en la interfaz
+    function showUsers() {
+        var userList = document.getElementById("user-list");
+
+        // Limpiamos la lista antes de mostrar los usuarios
+        userList.innerHTML = "";
+
+        // Iteramos sobre los usuarios y los mostramos en la lista
+        users.forEach(function (user) {
+            var listItem = document.createElement("li");
+            listItem.innerText = `${user.name}, ${user.age} años`;
+            userList.appendChild(listItem);
+        });
+    }
+
+    // Llamamos a la función para mostrar usuarios al cargar la página
+    showUsers();
+});
