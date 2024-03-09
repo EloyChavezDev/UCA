3d025bec46424c06902d873d30c0e0bceca55de0
EloyChavezDev <laneloychavez@gmail.com>
2024-03-09 11:25:54 -0600

    Creacion de BD

---
+++ js/productos.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'productos.json');
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    const productos = JSON.parse(xhr.responseText);
+    // Aquí puedes usar la información de los productos
+  } else {
+    console.error('Error al cargar la base de datos');
+  }
+};
+xhr.send();
